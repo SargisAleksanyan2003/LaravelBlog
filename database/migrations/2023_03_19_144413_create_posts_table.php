69ad700343238121dfe3e109150fd237cd36384c
SargisAleksanyan2003 <79356839+SargisAleksanyan2003@users.noreply.github.com>
2023-04-02 22:53:50 +0300

    Make table categories

--- database/migrations/2023_03_19_144413_create_posts_table.php
+++ database/migrations/2023_03_19_144413_create_posts_table.php
@@ -4,31 +4,36 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-	/**
-	 * Run the migrations.
-	 */
-	public function up(): void
-	{
-		Schema::create('posts', function (Blueprint $table) {
-			$table->id();
-			$table->string('title');
-			$table->text('text');
-			$table->string('image')->nullable();
-			$table->unsignedBigInteger('likes')->nullable();
-			$table->boolean('is_published')->default(1);
-			$table->timestamps();
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('text');
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('likes')->nullable();
+            $table->boolean('is_published')->default(1);
+            $table->timestamps();
 
             $table->softDeletes();
-		});
-	}
 
-	/**
-	 * Reverse the migrations.
-	 */
-	public function down(): void
-	{
-		Schema::dropIfExists('posts');
-	}
+            $table->unsignedBigInteger('category_id')->nullable();
+
+            $table->index('category_id', 'post_category_idx');
+
+            $table->foreign('category_id', 'post_category_')->on('categories')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
 };
